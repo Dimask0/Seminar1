6c2fd328d078cc5bddf81b95b56c3ebde31ccada
Dimask0 <2352713dk@mail.ru>
2023-06-09 22:08:46 +0300

    Seminar1

---
+++ task5/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Write("Введите число N ");
+int N = Convert.ToInt32(Console.ReadLine());
+int count = 0;
+while(count<2*N+1){
+if(count<N){
+    Console.Write($"{-N + count}");
+}
+else if (count == N){
+    Console.Write("0");
+}
+else {
+    Console.Write($"{count - N}");
+}
+count++;
+}
---
+++ task7/Program.cs
@@ -0,0 +1,4 @@
+﻿Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+// int lastDigit = number % 10;
+System.Console.WriteLine($"{number} -> {number % 10}");
