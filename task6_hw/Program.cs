3fdbfcfc41a83fff71c452991264b495436e55cb
Dimask0 <2352713dk@mail.ru>
2023-06-12 17:04:30 +0300

    Task6 ready

---
+++ task6_hw/Program.cs
@@ -0,0 +1,15 @@
+﻿// 4 -> да
+// -3 -> нет
+// 7 -> нет
+
+Console.Write ("Введите число: ");
+int A = Convert.ToInt32(Console.ReadLine());
+int remDiv = A % 2;
+if (remDiv == 0)
+{
+    Console.WriteLine ("Да");
+}
+else
+{
+    Console.WriteLine ("Нет");
+}
