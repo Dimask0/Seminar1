b2c083bf3f79cc7e72b48661a1ec14be0c590a54
Dimask0 <2352713dk@mail.ru>
2023-06-12 17:15:31 +0300

    Task8 ready

---
+++ task8_hw/Program.cs
@@ -0,0 +1,14 @@
+﻿// 5 -> 2, 4
+// 8 -> 2, 4, 6, 8
+
+Console.Write ("Введите число: ");
+int A = Convert.ToInt32(Console.ReadLine());
+int evenA = 2;
+if (A > 1)
+{
+   while (evenA <= A)
+   {
+    Console.Write (evenA + " ");
+    evenA = evenA + 2 ;
+   } 
+}
