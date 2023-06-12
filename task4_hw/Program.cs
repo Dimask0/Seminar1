b7f3ed5c72716a476d6d013120b453e5feacfb01
Dimask0 <2352713dk@mail.ru>
2023-06-12 16:43:13 +0300

    Task4 ready

---
+++ task4_hw/Program.cs
@@ -0,0 +1,24 @@
+﻿// 2, 3, 7 -> 7
+// 44 5 78 -> 78
+// 22 3 9 -> 22
+
+Console.WriteLine("Введите число");
+int A = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число: ");
+int B = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число: ");
+int C = Convert.ToInt32(Console.ReadLine());
+int max = 0;
+if (A > max)
+{
+    max = A;
+}
+if  (B > max)
+{
+    max = B;
+}
+if  (C > max)
+{
+    max = C;
+}
+Console.WriteLine ("max = " + max);
