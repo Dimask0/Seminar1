a57b299cb9483aa3c77ae3b2f0747e2296696283
Dimask0 <2352713dk@mail.ru>
2023-06-12 16:10:31 +0300

    Task2 ready

---
+++ task2_hw/Program.cs
@@ -0,0 +1,16 @@
+﻿// a = 5; b = 7 -> max = 7
+// a = 2 b = 10 -> max = 10
+// a = -9 b = -3 -> max = -3
+
+Console.WriteLine("Введите число");
+int A = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число: ");
+int B = Convert.ToInt32(Console.ReadLine());
+if (A > B)
+{
+    System.Console.WriteLine($"A = max ");
+}
+else
+{
+    System.Console.WriteLine($"B = max ");
+}
