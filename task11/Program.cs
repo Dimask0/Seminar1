a178de4cce935d8e0a609bd24d566923347459e2
Dimask0 <2352713dk@mail.ru>
2023-06-14 00:02:40 +0300

    Seminar 2

--- task11/Program.cs
+++ task11/Program.cs
@@ -5,16 +5,13 @@
 
 
 
-int DigitCrop (int num1)
+int SecondDigitDelete(int num)
 {
-  int digit1=num1/100;
-  int digit3=num1%10;
-  return digit1*10+digit3;
-
+int digit1 = num / 100;
+int digit3 = num % 10;
+int result = digit1 * 10 + digit3;
+return result;
 }
-
-
-
 int number = new Random().Next(100, 1000);
 System.Console.WriteLine($"Ваше случайное число: {number}");
-System.Console.WriteLine($"Число {number} после обрезания - {DigitCrop(number)}");
+System.Console.WriteLine(SecondDigitDelete(number));
