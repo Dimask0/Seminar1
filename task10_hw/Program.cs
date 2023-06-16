2d82fad9ed1f0d0c11e67f9b043ca835b3a1fa02
Dimask0 <2352713dk@mail.ru>
2023-06-16 18:43:24 +0300

    Homework 2

--- task10_hw/Program.cs
+++ task10_hw/Program.cs
@@ -3,3 +3,10 @@
 // 456-> 5
 // 782-> 8
 // 918-> 1
+
+Console.Clear();
+Console.WriteLine("Задача 10 ");
+Console.Write("Введи трёхзначное число: ");
+int threeDigitNumber = Convert.ToInt32(Console.ReadLine());
+string stringNumber = Convert.ToString(threeDigitNumber);
+Console.WriteLine("вторая цифра этого числа -> "+stringNumber[1]);
--- task15_hw/Program.cs
+++ task15_hw/Program.cs
@@ -2,3 +2,18 @@
 // 6 -> да
 // 7 -> да
 // 1 -> нет
+
+Console.Write("Введи цифру, обозначающую день недели: ");
+int dayNumber = Convert.ToInt32(Console.ReadLine());
+
+void CheckingTheDayOfTheWeek (int dayNumber) {
+  if (dayNumber == 6 || dayNumber == 7) {
+  Console.WriteLine("этот день выходной -> да");
+  }
+  else if (dayNumber < 1 || dayNumber > 7) {
+    Console.WriteLine("такого дня нет в недели");
+  }
+  else Console.WriteLine("(этот будний день) -> нет");
+}
+
+CheckingTheDayOfTheWeek(dayNumber);
