b360d64cd6578786b388f282b8f7af1857b76cd7
Dimask0 <2352713dk@mail.ru>
2023-06-18 18:50:09 +0300

    Homework 3

--- task21_hw/Program.cs
+++ task21_hw/Program.cs
@@ -17,9 +17,13 @@
 // Комбинация
 // System.Console.WriteLine(Math.Round(Math.Sqrt(Math.Pow(num, 3)), 2));
 
-double Distance2D(double Ax, double Ay, double Bx, double By)
+// Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+// A (3,6,8); B (2,1,-7), -> 15.84
+// A (7,-5, 0); B (1,-1,9) -> 11.53
+
+double Distance3D(double Ax, double Ay, double Bx, double By, double Az, double Bz)
 {
-    double distance = Math.Sqrt(Math.Pow(Ax - Bx, 2) + Math.Pow(Ay - By, 2));
+    double distance = Math.Sqrt(Math.Pow(Ax - Bx, 2) + Math.Pow(Ay - By,2)+ Math.Pow(Az - Bz,2));
     return Math.Round(distance, 2);
 }
 
@@ -27,12 +31,17 @@ System.Console.Write("Введите координату Х точки А: ");
 double Ax = Convert.ToInt32(Console.ReadLine());
 System.Console.Write("Введите координату Y точки А: ");
 double Ay = Convert.ToInt32(Console.ReadLine());
+System.Console.Write("Введите координату Z точки А: ");
+double Az = Convert.ToInt32(Console.ReadLine());
 
 System.Console.Write("Введите координату Х точки B: ");
 double Bx = Convert.ToInt32(Console.ReadLine());
 System.Console.Write("Введите координату Y точки B: ");
 double By = Convert.ToInt32(Console.ReadLine());
+System.Console.Write("Введите координату Z точки B: ");
+double Bz = Convert.ToInt32(Console.ReadLine());
 
-System.Console.WriteLine($"Расстояние между точками A и B равно {Distance2D(Ax, Ay, Bx, By)}");
+
+System.Console.WriteLine($"Расстояние между точками A и B равно {Distance3D(Ax, Ay, Bx, By, Az, Bz)}");
 
 
