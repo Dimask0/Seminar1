759b1b84680d40c49bafe13ca47c0a66351fa5b5
Dimask0 <2352713dk@mail.ru>
2023-06-30 21:50:49 +0300

    Homework 6

--- task59/Program.cs
+++ task59/Program.cs
@@ -9,6 +9,29 @@
 // 9 4 2
 // 2 2 6
 // 3 4 7
+void FillMatrixWithRandom(int[,] matrix)
+{
+    Random rnd = new Random();
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rnd.Next(0, 10);
+        }
+    }
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            System.Console.Write($"{matrix[i, j]} \t");
+        }
+        System.Console.WriteLine();
+    }
+}
 
 int [,] DeleteMinColRow (int [,] matrix)
 {
