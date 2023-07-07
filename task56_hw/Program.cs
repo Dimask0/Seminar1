a1b27d40275dc43c873aaba2ffcb41733b3127ff
Dimask0 <2352713dk@mail.ru>
2023-07-07 19:27:05 +0300

    Homework 8

---
+++ task56_hw/Program.cs
@@ -0,0 +1,63 @@
+﻿
+// Задача 56: Задайте прямоугольный двумерный массив. Напишите программу, которая будет находить строку с наименьшей суммой элементов.
+
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// 5 2 6 7
+// Программа считает сумму элементов в каждой строке и выдаёт номер строки с наименьшей суммой элементов: 1 строка
+
+void FillMatrixWithRandom(int[,] matrix)
+{
+    Random rnd = new Random();
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rnd.Next(0, 10);
+        }
+    }
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            System.Console.Write($"{matrix[i, j]} \t");
+        }
+        System.Console.WriteLine();
+    }
+}
+int FindMinRow(int[,] matrix)
+{
+  int [] rowSum = new int [matrix.GetLength(0)];
+  for (int i = 0; i < matrix.GetLength(0); i++)
+  {
+    for (int j = 0; j <matrix.GetLength(1); j++)
+    {
+      rowSum[i]=rowSum[i]+matrix[i,j];
+    }
+
+  }
+  int minRow = 0;
+  for (int i = 1; i < matrix.GetLength(0); i++)
+  {
+    if (rowSum[i]<rowSum[minRow])
+    {
+      minRow=i;
+    }
+  }
+return minRow;
+}
+System.Console.WriteLine("Введите кол-во строк: ");
+int row = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine("Введите кол-во столбцов: ");
+int column = Convert.ToInt32(Console.ReadLine());
+int[,] matrix = new int[row, column];
+FillMatrixWithRandom(matrix);
+PrintMatrix(matrix);
+System.Console.WriteLine();
+System.Console.WriteLine($"Минимальная строка под номером: {FindMinRow(matrix)}");
---
+++ task57/Program.cs
@@ -0,0 +1,63 @@
+﻿// Задача 57: Составить частотный словарь элементов двумерного массива. Частотный словарь содержит информацию о том, сколько раз встречается элемент входных данных.
+// 1, 2, 3 
+// 4, 6, 1 
+// 2, 1, 6
+// 1 встречается 3 раза 
+// 2 встречается 2 раз 
+// 3 встречается 1 раз 
+// 4 встречается 1 раз 
+// 6 встречается 2 раза
+
+void FillMatrixWithRandom(int[,] matrix)
+{
+    Random rnd = new Random();
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rnd.Next(0, 10);
+        }
+    }
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            System.Console.Write($"{matrix[i, j]} \t");
+        }
+        System.Console.WriteLine();
+    }
+}
+void PrintElementCount(int[,] matrix)
+    {
+    int count = 0;
+
+    for (int z = 0 ; z < 10; z++)
+    {
+    count = 0;
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if (z == matrix[i,j])
+            {
+            count++;
+            }
+        }
+        System.Console.WriteLine();
+    }
+    System.Console.WriteLine($"Элемент {z} встречается {count} раз");
+    }
+    }
+System.Console.WriteLine("Введите кол-во строк: ");
+int row = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine("Введите кол-во столбцов: ");
+int column = Convert.ToInt32(Console.ReadLine());
+int[,] matrix = new int[row, column];
+FillMatrixWithRandom(matrix);
+PrintMatrix(matrix);
+PrintElementCount(matrix);
+
